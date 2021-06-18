b17b8d7cc2898d888919627af74b53b0fbef4326
Betemariam Tegie <bete241011@gmail.com>
2021-06-18 08:51:20 -0700

    Fixed to have the correct out put.

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,25 +9,27 @@ int main(void)
 {
 	int i;
 	int j;
+	int i_first_digit;
+	int i_last_digit;
+	int j_first_digit;
+	int j_last_digit;
 
 	for (i = 0; i < 100; i++)
 	{
+		i_first_digit = (i / 10 + '0');
+		i_last_digit = (i % 10 + '0');
 		for (j = 0; j < 100; j++)
 		{
+			j_first_digit = (j / 10 + '0');
+			j_last_digit = (j % 10 + '0');
 			if (i != j && i < j)
 			{
-				if (i < 10)
-				{
-					putchar('0');
-				}
-				putchar(i % 10 + '0');
+				putchar(i_first_digit);
+				putchar(i_last_digit);
 				putchar(' ');
-				if (j < 10)
-				{
-					putchar('0');
-				}
-				putchar(j % 10 + '0');
-				if (i != 88)
+				putchar(j_first_digit);
+				putchar(j_last_digit);
+				if (i != 98)
 				{
 					putchar(',');
 					putchar(' ');
